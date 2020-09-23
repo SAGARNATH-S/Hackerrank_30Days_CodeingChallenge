e724fe1b89d35d5ccbbd4d2950f38a2a4fcde67a
SAGARNATH S <69468896+SAGARNATH-S@users.noreply.github.com>
2020-09-23 13:03:00 +0530

    Day-2 Condition

---
+++ Day-2.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    if N>=2:
+        if N<=5:
+            if N % 2 == 0 and N != 0:
+                print("Not Weird")
+            else:
+                print("Weird")
+    if N>=6:
+        if N<=20:
+            if N % 2 == 0:
+                print("Weird")
+    if N>20:
+        if N % 2 == 0:
+            print("Not Weird")
+        else: 
+            print("Weird")
+
+    if N>=1:
+        if N<2:
+            if N%2 != 0:
+                print("Weird")
+            else:
+                print("Not Weird")
+
+
